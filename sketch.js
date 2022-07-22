53505f2fd7c24cf5fc1d883204e27be54c38d840
LucasRorisCube <83036146+LucasRorisCube@users.noreply.github.com>
2022-07-22 11:47:18 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,24 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400,400);
 }
+
+let x=0
 
 function draw() {
   background(220);
-  line(10,10,200,200)
+  line(mouseX,mouseY,width/2,height/2);
+  if(x == 1){
+    line(20,20,300,300);
+  }
+  
+}
+
+function desenhar(){
+  line(20,20,300,300);
+  print("oi");
+  if(x == 0){
+    x = 1;
+  } else {
+    x = 0;
+  }
 }
